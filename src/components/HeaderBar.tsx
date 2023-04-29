94f2680c5f00380513f874e99c1b1261ec4fb89a
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-29 17:39:30 -0400

    Show HeaderBar when logged in

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,23 +1,48 @@
 import { useState, useEffect } from "react";
-import {
-  useUser,
-  useSupabaseClient,
-  Session,
-} from "@supabase/auth-helpers-react";
+import { useSupabaseClient, Session, User } from "@supabase/auth-helpers-react";
 import { Database } from "../types/supabase";
 import Avatar from "./Avatar";
+import Link from "next/link";
 
 type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
 
-export default function HeaderBar({ session }: { session: Session }) {
+export default function HeaderBar({
+  session,
+  user,
+}: {
+  session: Session;
+  user: User;
+}) {
   const supabase = useSupabaseClient<Database>();
-  const user = useUser();
 
   const [avatar_url, setAvatarUrl] = useState<Profiles["avatar_url"]>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
   useEffect(() => {
     getProfile();
   }, [session]);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      const menu = document.querySelector("#usermenu") as HTMLElement;
+      const menuToggle = document.querySelector(
+        "#usermenu-toggle"
+      ) as HTMLElement;
+      const target = event.target as HTMLElement;
+
+      if (menu && !menu.contains(target) && !menuToggle.contains(target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
 
   async function getProfile() {
     try {
@@ -25,7 +50,7 @@ export default function HeaderBar({ session }: { session: Session }) {
 
       let { data, error, status } = await supabase
         .from("profiles")
-        .select(`username, website, avatar_url`)
+        .select("username, avatar_url")
         .eq("id", user.id)
         .single();
 
@@ -35,6 +60,8 @@ export default function HeaderBar({ session }: { session: Session }) {
 
       if (data) {
         setAvatarUrl(data.avatar_url);
+        setUsername(user.user_metadata.full_name);
+        setUserEmail(user.email);
       }
     } catch (error) {
       alert("Error loading user data!");
@@ -43,12 +70,35 @@ export default function HeaderBar({ session }: { session: Session }) {
   }
 
   return (
-    <div className="flex items-center">
-      <Avatar url={avatar_url} size={50} />
-
-      <button className="button block" onClick={() => supabase.auth.signOut()}>
-        Sign Out
+    <div className="flex h-14 items-center justify-end border border-b-neutral-200 px-4 relative">
+      <Link href="/" className="mr-auto font-semibold text-lg">
+        {" "}
+        Healthsimple{" "}
+      </Link>
+      <button onClick={() => setIsMenuOpen(!isMenuOpen)} id="usermenu-toggle">
+        <Avatar url={avatar_url} size={32} />
       </button>
+
+      {isMenuOpen && (
+        <div
+          id="usermenu"
+          className="absolute right-4 top-[90%] bg-white border border-neutral-200 rounded-md shadow-lg"
+        >
+          <div className="px-4 py-2 flex flex-col border-b border-gray-200">
+            <span className="font-semibold">{username}</span>
+            <span className="text-sm">{userEmail}</span>
+          </div>
+
+          <div className="py-1 flex flex-col">
+            <button
+              onClick={() => supabase.auth.signOut()}
+              className="block px-4 py-2 text-gray-700 hover:bg-neutral-100 text-left"
+            >
+              Log out
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,33 @@
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import {
+  useSession,
+  useSupabaseClient,
+  useUser,
+} from "@supabase/auth-helpers-react";
 import HeaderBar from "../components/HeaderBar";
 
 const Home = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
+  const user = useUser();
 
   return (
     <div className="w-full h-full flex-1">
       {!session ? (
         <div className="max-w-[20rem] mx-auto mt-60">
+          <div className="text-center font-semibold text-xl ">Healthsimple</div>
           <Auth
             supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            theme="dark"
+            appearance={{
+              theme: ThemeSupa,
+            }}
             providers={["google"]}
           />
         </div>
       ) : (
         <div>
-          <HeaderBar session={session} />
+          <HeaderBar session={session} user={user} />
         </div>
       )}
     </div>
