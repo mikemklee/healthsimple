e564933eb3c0248c8a56addd2d6e02fd582228a6
Mike Lee <mikemunkyu.lee@gmail.com>
2023-05-03 21:03:53 -0400

    Tweak supabase theme colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({
   Component,
@@ -13,12 +14,17 @@ function MyApp({
   const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabase}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} />
-    </SessionContextProvider>
+    <>
+      <Head>
+        <title>Healthsimple</title>
+      </Head>
+      <SessionContextProvider
+        supabaseClient={supabase}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </>
   );
 }
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,11 +18,30 @@ const Home = () => {
     <div className="w-full h-full flex-1">
       {!authorized ? (
         <div className="max-w-[20rem] mx-auto mt-60">
-          <div className="text-center font-semibold text-xl ">Healthsimple</div>
+          <div className="text-center font-semibold text-2xl mb-6">
+            Healthsimple
+          </div>
           <Auth
             supabaseClient={supabase}
             appearance={{
               theme: ThemeSupa,
+              variables: {
+                default: {
+                  colors: {
+                    // brand: "#3498DB",
+                    // brandAccent: "#2E86C1",
+                    // brand: "#1ABC9C",
+                    // brandAccent: "#17A589",
+                    brand: "#16A085",
+                    brandAccent: "#138D75",
+                    brandButtonText: "#f7f9f9",
+                    defaultButtonBackground: "#f7f9f9",
+                    defaultButtonBackgroundHover: "#ECF0F1",
+                    defaultButtonBorder: "#D0D3D4",
+                    defaultButtonText: "gray",
+                  },
+                },
+              },
             }}
             providers={["google"]}
           />
