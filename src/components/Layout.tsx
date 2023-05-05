0186313cd71ffee9e77f2c9d4ed2c3487ae04d4b
Mike Lee <mikemunkyu.lee@gmail.com>
2023-05-04 22:33:29 -0400

    Fix TS error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-function Layout({ children }) {
+type Props = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: Props) {
   return (
     <div className="w-full h-full flex-1 mt-14 p-4 mx-auto flex flex-col max-w-[60rem]">
       {children}
