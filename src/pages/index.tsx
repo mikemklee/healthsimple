99004f78f6b859799cfc489b20aa7cd7b6b42831
Mike Lee <mikemunkyu.lee@gmail.com>
2023-05-04 22:48:37 -0400

    Add utility dir

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import { useAuthGuard } from "@/hooks/useAuthGuard";
+import InterfaceUtils from "@/utils/interface";
 
 const Home = () => {
   const { isSessionLoading, session } = useAuthGuard();
 
   if (isSessionLoading || !session) return null;
 
+  const user = session.user;
+  const userName = user?.user_metadata?.name;
+
   return (
     <>
-      <div>Hello!</div>
+      <div className="text-xl my-4">
+        Good {InterfaceUtils.getTimeOfDay()},{" "}
+        <span className="font-semibold">{userName}</span>
+      </div>
     </>
   );
 };
---
+++ src/utils/interface.ts
@@ -0,0 +1,16 @@
+const InterfaceUtils = {
+  getTimeOfDay: () => {
+    const today = new Date();
+    const hours = today.getHours();
+
+    if (hours < 12) {
+      return "morning";
+    } else if (hours < 18) {
+      return "afternoon";
+    } else {
+      return "evening";
+    }
+  },
+};
+
+export default InterfaceUtils;
